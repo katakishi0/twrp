0ff7fdf0ea966c09bc6fdda04b6973cd4e171ee7
Khalil.B <80763333+katakishi0@users.noreply.github.com>
2024-02-29 02:09:13 -0300

    vai vim bootloop kkkkkkkkkkkkkkkkkk

--- device.mk
+++ device.mk
@@ -65,6 +65,11 @@ PRODUCT_PACKAGES += \
     bootctrl.$(PRODUCT_PLATFORM) \
     bootctrl.$(PRODUCT_PLATFORM).recovery
 
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
